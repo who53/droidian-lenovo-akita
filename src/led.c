a43532d74d112c9f52a6b909841fb100dd9777f9
Deepak Meena <who53@disroot.org>
2025-12-12 14:32:54 +0530

    refresh
    Signed-off-by: Deepak Meena <who53@disroot.org>

--- src/led.c
+++ src/led.c
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: GPL-2.0-or-later
- * Copyright (C) 2024 Deepak Kumar <notwho53@gmail.com>
+ * Copyright (C) 2024 Deepak Kumar <who53@disroot.org>
  */
 
 #include <stdio.h>
